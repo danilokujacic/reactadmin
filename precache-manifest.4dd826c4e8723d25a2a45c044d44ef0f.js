90f694bffba41afe9c2e9676a8c2c5a463bcfd83
Danilo <danilo.kujacic@bild-studio.net>
2020-05-11 13:03:14 +0200

    Updates

---
+++ precache-manifest.4dd826c4e8723d25a2a45c044d44ef0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6881c91b1041d3cb90431b427eed66",
+    "url": "/reactadmin/index.html"
+  },
+  {
+    "revision": "9184206e92a80fd30caf",
+    "url": "/reactadmin/static/css/main.d3a50fd1.chunk.css"
+  },
+  {
+    "revision": "5db216d27ea74d27188a",
+    "url": "/reactadmin/static/js/2.b4da4937.chunk.js"
+  },
+  {
+    "revision": "74302187dc1f033042b1164070a401c8",
+    "url": "/reactadmin/static/js/2.b4da4937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9184206e92a80fd30caf",
+    "url": "/reactadmin/static/js/main.f31abda8.chunk.js"
+  },
+  {
+    "revision": "ff7bf36df8c428f52e59",
+    "url": "/reactadmin/static/js/runtime-main.8e6943c9.js"
+  }
+]);
